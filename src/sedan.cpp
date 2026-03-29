328a04526136a15e801b5b53ac4784d0c87a9d8d
mjstriker <76749095+mjstriker@users.noreply.github.com>
2026-03-29 21:18:12 +0200

    done

--- src/sedan.cpp
+++ src/sedan.cpp
@@ -3,34 +3,48 @@
 
 static const std::array<double, 7> gear_ratios = {-1, 1, 2, 3, 4, 5, 6};
 
+// starter i index 0, da gear_ratios[0] = -1, hvilket er bakgear
 Sedan::Sedan() : current_gear(0)
 {
 }
 
 void Sedan::shift_up()
 {
+    // skifter gear til et højere gear hvis den ikke er max gear (6)
+    if (current_gear < 6)
+    {
+        current_gear++;
+    }
 }
 
 void Sedan::shift_down()
 {
+    // skifter gear til et lavere gear hvis den ikke er 0
+    if (current_gear > 0)
+    {
+        current_gear--;
+    }
 }
 
 double Sedan::get_torque(double engine_rpm, double throttle)
 {
-    return 0;
+    // test model med 100Nm * throttle
+    return 100 * throttle;
 }
 
 double Sedan::get_weight()
 {
-    return 1;
+    // test model med 2000 (den som var fra eksempel i opgaven)
+    return 2000;
 }
 
 double Sedan::get_wheel_radius()
 {
-    return 1;
+    // test model med 0.3 (den som var fra eksempel i opgaven)
+    return 0.3;
 }
 
 double Sedan::get_current_gear_ratio()
 {
-    return 1;
+    return gear_ratios[current_gear];
 }
